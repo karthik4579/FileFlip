77e50fb2a5cf04054a3fcab6ca9a694910625a36
karthik nair <90816230+karthik4579@users.noreply.github.com>
2024-04-02 02:48:39 +0530

    FileFlip : Rmove the section to run unoserver in the background

--- run.py
+++ run.py
@@ -3,13 +3,6 @@ import psutil
 from pathlib import Path
 
 process_names = [proc.name() for proc in psutil.process_iter()]
-
-
-# Check if process name matches unoserver
-if 'unoserver' in process_names:
-      pass
-else:
-    os.system('unoserver >> /dev/null &')
     
 if Path(f"{Path.cwd()}/temp_files/input").exists() and Path(f"{Path.cwd()}/temp_files/output").exists():
      pass
